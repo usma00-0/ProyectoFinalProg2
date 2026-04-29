54331ff3876902ed786fdaa148f5e93adf548035
usma00-00 <usmajuan0012@gmail.com>
2026-04-29 18:26:37 -0500

    Corrección en NodoCliente y Agregación de ConsulClientes

--- NodoCliente.cs
+++ NodoCliente.cs
@@ -7,6 +7,15 @@ public class NodoCliente
     public int Saldo { get; set; }
 
     public NodoCliente Siguiente { get; set; }
+    public int Id { get; set; }
 
-    
+    public NodoCliente(int cedula, string nombre, string apellido, int numCuenta, int saldo)
+    {
+        Cedula = cedula;
+        Nombre = nombre;
+        Apellido = apellido;
+        NumCuenta = numCuenta;
+        Saldo = saldo;
+        Siguiente = null;
+    }
 }
