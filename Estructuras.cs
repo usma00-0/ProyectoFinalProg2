6312051ed9a8c357cc5d6ac987e29e1a14fe8eb5
usma00-00 <usmajuan0012@gmail.com>
2026-05-06 23:13:09 -0500

    Adelanto de estructura

--- Estructuras.cs
+++ Estructuras.cs
@@ -2,5 +2,31 @@ public class ListaEnlazadaClientes
 {
     private NodoCliente cabeza;
 
-    
+    public bool AgregarCliente(int cedula, string nombre, string apellido, int numCuenta, int saldo)
+    {
+        [cite_start]//No permite duplicados
+        if(BuscarClientePorCedula(cedula) != null || BuscarClientePorCuenta(numCuenta) != null)
+        {
+            Console.WriteLine("Ya existe un cliente con esa cédula o número de cuenta");
+            return false;
+        }
+
+        NodoCliente nuevoCliente = new NodoCliente(cedula, nombre, apellido, numCuenta, saldo);
+
+        if(cabeza == null)
+        {
+          cabeza = nuevoCliente;  
+        }
+        else
+        {
+            NodoCliente actual = cabeza;
+            while(actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevoCliente;
+        }
+        Console.WriteLine("Cliente agregado exitosamente");
+        return true;
+    }
 }
